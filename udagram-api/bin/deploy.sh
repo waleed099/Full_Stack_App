81c930002965cd962c026bf6818eea2a50e25062
waleedsaeeed <w.saeeed123@gmail.com>
2023-11-28 12:10:08 +0300

    Update  commit

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -8,4 +8,4 @@ eb health
 # if you want read the logs
 # eb logs
 
-eb setenv AWS_ACCESS_KEY_ID="$AKIA55N4WRZCDN7YBF6U" AWS_SECRET_ACCESS_KEY="$hTjxcxFeCQ6+qvrrjsGxw/QbTkUDsaJ23yUceTte" AWS_DEFAULT_REGION="$us-west-2" POSTGRES_USERNAME="$POSTGRES_USERNAME" POSTGRES_PASSWORD="$postgres" POSTGRES_DB="$postgres" PORT="$5432" POSTGRES_HOST="$udagram-api.cv1ohyyc0bed.us-west-2.rds.amazonaws.com" AWS_REGION="$AWS_REGION" AWS_PROFILE="$AWS_PROFILE" AWS_BUCKET="$AWS_BUCKET" URL="$http://udagramfullstack-env.eba-zw5vyd5m.us-west-2.elasticbeanstalk.com/" JWT_SECRET="$mysecretstring" --profile default
+eb setenv AWS_ACCESS_KEY_ID="$AKIA55N4WRZCDN7YBF6U" AWS_SECRET_ACCESS_KEY="$hTjxcxFeCQ6+qvrrjsGxw/QbTkUDsaJ23yUceTte" AWS_DEFAULT_REGION="$us-west-2" POSTGRES_USERNAME="$postgres" POSTGRES_PASSWORD="$postgres" POSTGRES_DB="$postgres" PORT="$5432" POSTGRES_HOST="$udagram-api.cv1ohyyc0bed.us-west-2.rds.amazonaws.com" AWS_REGION="$AWS_REGION" AWS_PROFILE="$us-west-2" AWS_BUCKET="$elasticbeanstalk-us-west-2-956562837060" URL="$http://udagramfullstack-env.eba-zw5vyd5m.us-west-2.elasticbeanstalk.com/" JWT_SECRET="$mysecretstring" --profile default
