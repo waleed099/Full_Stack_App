a3a95822e107b8765cb2b977489077d1f5f92f99
waleedsaeeed <w.saeeed123@gmail.com>
2023-12-04 15:08:47 +0300

    Initial commitss

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -8,4 +8,4 @@ eb health
 # if you want read the logs
 # eb logs
 
-eb setenv AWS_ACCESS_KEY_ID="$AKIA55N4WRZCOWAZ54MG" AWS_SECRET_ACCESS_KEY="$tHHqmokrgt7+jXAX5PO60m7Ctfulrme+hbVYzM99" AWS_DEFAULT_REGION="$us-west-2" POSTGRES_USERNAME="$postgres" POSTGRES_PASSWORD="$postgres" POSTGRES_DB="$postgres" PORT="$5432" POSTGRES_HOST="$udagram-api.cv1ohyyc0bed.us-west-2.rds.amazonaws.com" AWS_REGION="$us-west-2a" AWS_PROFILE="$default" AWS_BUCKET="$aws-bucket0996985194" URL="$http://aws-bucket0996985194.s3-website-us-west-2.amazonaws.com/home" JWT_SECRET="$mysecretstring" --profile default
+eb setenv AWS_ACCESS_KEY_ID="$AKIA55N4WRZCAR3255W5" AWS_SECRET_ACCESS_KEY="$ufCurcefNRAvt4GrPuxKYotycfItS3bRFJYXjZBk" AWS_DEFAULT_REGION="$us-west-2" POSTGRES_USERNAME="$postgres" POSTGRES_PASSWORD="$postgres" POSTGRES_DB="$postgres" PORT="$5432" POSTGRES_HOST="$udagram-api.cv1ohyyc0bed.us-west-2.rds.amazonaws.com" AWS_REGION="$us-west-2a" AWS_PROFILE="$default" AWS_BUCKET="$aws-bucket0996985194" URL="$http://aws-bucket0996985194.s3-website-us-west-2.amazonaws.com/home" JWT_SECRET="$mysecretstring" --profile default
